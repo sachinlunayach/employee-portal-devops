10eb22a0269a857948820bc66b50450b08d8467c
sachinlunayach <sachinlunayach333@gmail.com>
2026-06-26 17:39:28 +0530

    Sprint 3: Frontend connected with Flask API

--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,42 @@
-document
-.getElementById("searchBtn")
-.addEventListener("click",()=>{
+const button = document.getElementById("searchBtn");
+const result = document.getElementById("result");
 
-document
-.getElementById("result")
-.innerHTML=
+button.addEventListener("click", async () => {
 
-"Backend coming in Sprint-2 🚀";
+    const employeeId = document.getElementById("employeeId").value;
+
+    if (!employeeId) {
+        result.innerHTML = "<p>Please enter Employee ID</p>";
+        return;
+    }
+
+    try {
+
+        const response = await fetch(
+            `http://127.0.0.1:5000/employee?id=${employeeId}`
+        );
+
+        if (!response.ok) {
+            throw new Error("Employee Not Found");
+        }
+
+        const employee = await response.json();
+
+        result.innerHTML = `
+            <h3>${employee.name}</h3>
+            <p><strong>ID:</strong> ${employee.id}</p>
+            <p><strong>Role:</strong> ${employee.role}</p>
+            <p><strong>Salary:</strong> ${employee.salary}</p>
+        `;
+
+    } catch (error) {
+
+        result.innerHTML = `
+            <p style="color:red;">
+                ${error.message}
+            </p>
+        `;
+
+    }
 
 });
